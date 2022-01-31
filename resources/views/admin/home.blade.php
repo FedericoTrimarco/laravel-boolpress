1616027d40d476935afcbbbf0edfb8c9ef974ba6
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-31 15:57:07 +0100

    add Rout::middleware && front rout

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Welcome to {{ Auth::user()->name }} dashboard</h1>
+                </div>
+
+                <div class="card-body">
+                    <h2>What to do next?</h2>
+                    <ul>
+                        <li>
+                            <a href="">Lorem ipsum dolor</a>
+                        </li>
+                        <li>
+                            <a href="">Lorem ipsum dolor</a>
+                        </li>
+                        <li>
+                            <a href="">Lorem ipsum dolor</a>
+                        </li>
+                        <li>
+                            <a href="">Lorem ipsum dolor</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

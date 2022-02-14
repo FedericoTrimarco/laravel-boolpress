deae8407b8d32f86a4e233d64ed327d8691353c7
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-02-14 22:35:51 +0100

    add send email

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,9 +5,25 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\SendWelcomeEmail;
+
 class HomeController extends Controller
 {
     public function index(){
+
+        // Test invio email
+        
+        // a. dati statici
+        // Mail::to('account@mail.it')->send(new SendWelcomeEmail());
+
+        // b. con auth
+        // Mail::to(Auth::user()->email)->send(new SendWelcomeEmail());
+
+        // c. con passaggio dati a classe  -> vista x email
+        Mail::to(Auth::user()->email)->send(new SendWelcomeEmail(Auth::user()->name));
+
         return view('admin.home');
     }
 }
---
+++ resources/views/mails/welcome.blade.php
@@ -0,0 +1,3 @@
+<h1>WELCOME {{ $name }}</h1>
+
+<p>grazie per esserti inscritto al nostro sito. Riceverai ulteriori informazioni in seguito. <br> Cordiali saluti, BoolPress.</p>
